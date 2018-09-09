112c0576e668baee824ef849541e92598f1d6f0d
subratliku <subrat1874@gmail.com>
2018-09-09 23:57:18 +0530

    updated flipkartdropdown.py with comment for sleep

--- flipkart dropmenu.py
+++ flipkart dropmenu.py
@@ -7,7 +7,7 @@ driver=Chrome('C:\selenium-Project-Training\Drivers\chromedriver.exe')
 
 driver.implicitly_wait(30)
 driver.get('https://www.flipkart.com')
-time.sleep(5)
+time.sleep(5) #this 5 sec sleep to close the pop up so that we can prform move_to_element Electronics
 dropmenu_element=driver.find_element_by_xpath("//span[contains(text(),'Electronics')]")
 act = ActionChains(driver=driver)
 act.move_to_element(dropmenu_element).perform()
